a9b41d3c3537860cc1d350660cf4ac881bd62d45
imattf <imattf@pm.me>
2020-06-27 07:40:30 -0700

    added postgres implementation

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ type datastore interface {
 	get(n int) person     //pass in key & return a person value
 }
 
-func put(d datastore, n int, p person) {
+func store(d datastore, n int, p person) {
 	d.save(n, p)
 }
 func get(d datastore, n int) person {
@@ -46,13 +46,8 @@ func get(d datastore, n int) person {
 
 func main() {
 
-	//create the datastore
-	fmt.Println("Create datastore...")
-	mds := mongodb{}
-	//pds := postgres{}
-
-	//create the persons
-	fmt.Println(".start creating the data")
+	//create the persons data
+	fmt.Println("Creating the data...")
 	p1 := person{
 		first: "Bob",
 	}
@@ -66,11 +61,16 @@ func main() {
 	fmt.Println("...created:", p2)
 	fmt.Println("...created:", p3)
 
+	//create the datastores
+	fmt.Println("Create datastore...")
+	mds := mongodb{}
+	pds := postgres{}
+
 	//save the persons in a mongodb datastore
-	fmt.Println(".start loading data into mongodb datastore")
-	put(mds, 1, p1)
-	put(mds, 2, p2)
-	put(mds, 3, p3)
+	fmt.Println(".start storing data into mongodb datastore")
+	store(mds, 1, p1)
+	store(mds, 2, p2)
+	store(mds, 3, p3)
 	fmt.Println("...storing:", p1)
 	fmt.Println("...storing:", p2)
 	fmt.Println("...storing:", p3)
@@ -80,6 +80,24 @@ func main() {
 	rec1 := get(mds, 1)
 	rec2 := get(mds, 2)
 	rec3 := get(mds, 3)
+	fmt.Println("...getting:", rec1)
+	fmt.Println("...getting:", rec2)
+	fmt.Println("...getting:", rec3)
+
+	//store the persons in a postgres datastore
+	fmt.Println(".start storing data into postres datastore")
+	store(pds, 1, p1)
+	store(pds, 2, p2)
+	store(pds, 3, p3)
+	fmt.Println("...storing:", p1)
+	fmt.Println("...storing:", p2)
+	fmt.Println("...storing:", p3)
+
+	//get the persons from the postgres datastore
+	fmt.Println(".start getting data from postgres datastore")
+	rec1 = get(pds, 1)
+	rec2 = get(pds, 2)
+	rec3 = get(pds, 3)
 	fmt.Println("...getting:", rec1)
 	fmt.Println("...getting:", rec2)
 	fmt.Println("...getting:", rec3)

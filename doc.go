4a7e205f862bfb60557343f887c8261997add618
imattf <imattf@pm.me>
2020-06-27 14:09:46 -0700

    update doc.go

--- doc.go
+++ doc.go
@@ -1,5 +1,4 @@
-// This is a pckage that is an example of using interfaces to provide a Datastore and DatastoreService
+// Package architecture is an example of using interfaces to provide a Datastore and DatastoreService
 // Interfaces are contracts that will implicitly garauntee any value is going to have a method
-// Interface will go find the unique implmentation on the concrete type
-
+// Interface will go find the unique implementation on the concrete type
 package architecture

24b7d9baceca7063d2087ae952ca2af71c05237b
imattf <imattf@pm.me>
2020-06-27 14:23:26 -0700

    re-org documentation

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package architecture is an example of using interfaces to provide a Datastore and DatastoreService
-// Interfaces are contracts that will implicitly garauntee any value is going to have a method
-// Interface will go find the unique implementation on the concrete type
+// Package architecture is an example of using interfaces to provide a Datastore and DatastoreService.
+// Interfaces are contracts that will implicitly garauntee any value is going to have a method.
+// Interface will go find the unique implementation on the concrete type.
 package architecture
--- service.go
+++ service.go
@@ -2,32 +2,25 @@ package architecture
 
 import "fmt"
 
-// Person is how the architecture package stores a person
+// Person is how the architecture package stores a person.
 type Person struct {
 	First string
 }
 
-// Datastore is how to store and get a person
-// When getting a person, if they do not exist, return the zero value
+// Datastore is how to store and get a person.
+// When getting a person, if they do not exist, return the zero value.
 type Datastore interface {
 	Save(n int, p Person) //pass in key & Person value
 	Get(n int) Person     //pass in key & return a Person value
 }
 
-// DatastoreService is wrapper service to get records from a Datastore
+// DatastoreService is wrapper service to get records from a Datastore.
 type DatastoreService struct {
 	d Datastore
 }
 
-// NewDatastoreService creates a new DatastoreService
-func NewDatastoreService(d Datastore) DatastoreService {
-	return DatastoreService{
-		d: d,
-	}
-}
-
-// Get method takes a key value and returns a record value
-// If no key exists, an error is returned
+// Get method takes a key value and returns a record value.
+// If no key exists, an error is returned.
 func (ds DatastoreService) Get(n int) (Person, error) {
 	p := ds.d.Get(n)
 	if p.First == "" {
@@ -36,12 +29,19 @@ func (ds DatastoreService) Get(n int) (Person, error) {
 	return p, nil
 }
 
-// Store function takes a datastore, key value and a record value
+// NewDatastoreService function takes a Datastore and returns a new DatastoreService.
+func NewDatastoreService(d Datastore) DatastoreService {
+	return DatastoreService{
+		d: d,
+	}
+}
+
+// Store function takes a datastore, key value and a record value.
 func Store(d Datastore, n int, p Person) {
 	d.Save(n, p)
 }
 
-// Get function takes a datastore and key value and returns a record value
+// Get function takes a datastore and key value and returns a record value.
 func Get(d Datastore, n int) Person {
 	return d.Get(n)
 }

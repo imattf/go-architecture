133b8c687eff1aa22809ab1aa5e9ebed81f34d64
imattf <imattf@pm.me>
2020-06-28 16:48:21 -0700

    added Hand-on Exercise 2 - Ninja Level 2

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
-// Hand on Exercise 1 - Ninja Level 2
-// Create an interface and use it
+// Hand on Exercise 2 - Ninja Level 2
+// Recreate the database example using two maps to simulate the databases. 
+// Create an “accessor” interface that has two methods
+//  save
+//  retrieve
+// have your databases implement the “accessor” interface
+// create functions that take in values of type accessor
+//  put
+//  get
+// use “put” and “get” to store data in either database
+
 
 package main
 
@@ -7,70 +16,82 @@ import (
 	"fmt"
 )
 
-type animal struct {
-	name  string
-	walks bool
+// data store object
+type user struct {
+	first string
 }
 
-type dog struct {
-	animal
-	digs bool
+// data store type mongo
+type mongo map[int]user
+
+// data store save method
+func (m mongo) save(n int, u user) {
+	m[n] = u
+	fmt.Println("...saving in mongo:", m[n])
 }
 
-func (dg dog) speak() {
-	fmt.Println(dg.name, "says bark...")
-	fmt.Println("...and do I like walks?, ", dg.walks)
-	fmt.Println("...and do I dig in the yard?, ", dg.digs)
-}
-func (dg dog) showAffection() {
-	fmt.Println("...and I show affection by wagging my tail")
+// data store retrive method
+func (m mongo) retrieve(n int) user {
+	fmt.Println("...retrieving from mongo:", m[n])
+	return m[n]
 }
 
-type cat struct {
-	animal
-	hairball bool
+// data store type hard-drive
+type hdrive map[int]user
+
+// data store save method
+func (h hdrive) save(n int, u user) {
+	fmt.Println("...saving in hard-drive:", u)
+	h[n] = u
 }
 
-func (ct cat) speak() {
-	fmt.Println(ct.name, "says meow...")
-	fmt.Println("...and do I like walks?, ", ct.walks)
-	fmt.Println("...and do I pukes up hairballs?, ", ct.hairball)
-}
-func (ct cat) showAffection() {
-	fmt.Println("...and I show affection by purring")
+// data store retrive method
+func (h hdrive) retrieve(n int) user {
+	fmt.Println("...retrieving from hard-drive:", h[n])
+	return h[n]
 }
 
-type pet interface {
-	speak()
-	showAffection()
+// data store interface type
+type accessor interface {
+	save(n int, u user)
+	retrieve(n int) user
 }
 
-func petAnalysis(p pet) {
-	p.speak()
-	p.showAffection()
+// Put function takes an interface type accessor, stores a value 
+func put(a accessor, n int, u user) {
+	a.save(n, u)
+}
+
+// Get function takes an interface type accessor, gets a value
+func get(a accessor, n int) user {
+	return a.retrieve(n)
 }
 
 func main() {
-	pet1 := dog{
-		animal: animal{
-			name:  "Spot",
-			walks: true,
-		},
-		digs: true,
+
+	// create the data store type...
+	storage := mongo{}  //use a mongo mock
+	// storage := hdrive{} //use a hard-drive mock
+
+	// create values to store in data store...
+	u1 := user{
+		first: "James",
 	}
 
-	pet2 := cat{
-		animal: animal{
-			name:  "Tiger",
-			walks: false,
-		},
-		hairball: true,
+	u2 := user{
+		first: "Jenny",
 	}
 
-	fmt.Println("dog:", pet1)
-	fmt.Println("cat:", pet2)
+	// put the values in the data store..
+	fmt.Println("Putting values into data storage...")
+	put(storage, 1, u1)
+	put(storage, 2, u2)
 
-	petAnalysis(pet1)
-	petAnalysis(pet2)
+	// get the values in the data store..
+	fmt.Println("Getting values from data storage...")
+	// fmt.Println(get(storage, 1))
+	// fmt.Println(get(storage, 2))
+	get(storage, 1)
+	get(storage, 2)
 
 }

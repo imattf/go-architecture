a8c4be8f966401a961b720a9b6c6f18042951d4f
imattf <imattf@pm.me>
2020-06-28 15:12:33 -0700

    looked at testify

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/imattf/go-architecture
 
 go 1.13
 
-require github.com/GoesToEleven/golang-architecture v1.35.0 // indirect
+require (
+	github.com/GoesToEleven/golang-architecture v1.35.0 // indirect
+	github.com/stretchr/testify v1.6.1
+)
--- service_test.go
+++ service_test.go
@@ -3,10 +3,12 @@ package architecture
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+// Test setup...
 // Copy local database mock into here, otherwise we get a import error
-
 type Db map[int]Person
 
 func (mg Db) Save(n int, p Person) {
@@ -14,6 +16,23 @@ func (mg Db) Save(n int, p Person) {
 }
 func (mg Db) Get(n int) Person {
 	return mg[n]
+}
+
+func TestSomething(t *testing.T) {
+	// All these assertions pass
+	assert.Equal(t, "hello", "hello", "Values are equal")
+	// assert.NotEqual(t, "hello", "world", "Values are different")
+	// assert.Contains(t, "hello", "el", "String contains other given string")
+	// assert.True(t, true, "Value is true")
+	// assert.False(t, false, "Value is false")
+
+	// All these assertions fail
+	// assert.Equal(t, "hello", "world", "Values are equal")
+	// assert.NotEqual(t, "hello", "hello", "Values are different")
+	// assert.Contains(t, "hello", "y", "String contains other given string")
+	// assert.True(t, false, "Value is true")
+	// assert.False(t, true, "Value is false")
+
 }
 
 func TestPut(t *testing.T) {
@@ -28,8 +47,11 @@ func TestPut(t *testing.T) {
 		t.Fatalf("Want %v, got %v", p, got)
 	}
 
+	assert.True(t, true, "True is true!")
+
 }
 
+//this will create Example in go doc...
 func ExamplePut() {
 	mdb := Db{}
 	p := Person{
@@ -57,6 +79,7 @@ func TestGet(t *testing.T) {
 
 }
 
+//this will create Example in go doc...
 func ExampleGet() {
 	mdb := Db{}
 	p := Person{

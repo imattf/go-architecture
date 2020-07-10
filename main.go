dd75c52294d6661867088a95bab2ca5bc19444e0
imattf <imattf@pm.me>
2020-07-10 07:27:16 -0700

    hands-on #1 - ninja level 3

--- main.go
+++ main.go
@@ -1,33 +1,49 @@
-//Parent Context context.Background()
-// with a child context given a Value
-// a better approach
+//Hands-on Exercise #1 - Ninja Level 3
+// Create a background context.
+// Create a child context with that background as a parent
+// store two values in the child context
+// key of type string
+// key of type int
+// pull the values out
+// tag v1.13.0
+// video: 67 Hands On 1
 
 package main
 
 import (
 	"context"
 	"fmt"
-
-	"github.com/imattf/go-architecture/session"
 )
 
-type key int
+// type key int
 
-var userKey key = 0
+// var userKey key = 0
+// var admin key = 1
 
 func main() {
 
-	ctx := context.Background()       //no Value associated w/ context, so you get "not logged in"
-	ctx = session.WithUserID(ctx, 42) //you will be logged in
+	//create parent background context
+	ctx := context.Background()
 
-	userID := session.GetUserID(ctx)
-	if userID == nil {
-		fmt.Println("Not logged in!")
-		return
+	//create child context w/ 2 values
+	ctx = context.WithValue(ctx, "userID", 1234)
+
+	//append another value to the child context
+	ctx = context.WithValue(ctx, 1, "admin")
+
+	//pull the values out
+	val := ctx.Value("userID")
+	if val == nil {
+		fmt.Println("No value for key userID")
+	} else {
+		fmt.Println("userID =", val)
 	}
-	fmt.Println(*userID)
 
-	uID := ctx.Value(userKey)
-	fmt.Println(uID)
+	val = ctx.Value(1)
+	if val == nil {
+		fmt.Println("userID is not an admin")
+	} else {
+		fmt.Println("userID is an", val)
+	}
 
 }

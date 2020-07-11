49e625c711bf2e008ad15b60ad1417338b01f83d
imattf <imattf@pm.me>
2020-07-11 12:38:20 -0700

    Hands-on Exercise #2 - Ninja Level 3

--- main.go
+++ main.go
@@ -1,49 +1,30 @@
-//Hands-on Exercise #1 - Ninja Level 3
-// Create a background context.
-// Create a child context with that background as a parent
-// store two values in the child context
-// key of type string
-// key of type int
+//Hands-on Exercise #2 - Ninja Level 3
+// Using the code from the previous exercise
+// have each key be its own type
+// abstract all of the context code into its own
+// functions as applicable
+// package, separate from main
 // pull the values out
-// tag v1.13.0
-// video: 67 Hands On 1
+// tag v1.14.0 & v1.16.0
+// video 69 Hands On 2 - Attempt 1
 
 package main
 
 import (
 	"context"
 	"fmt"
+
+	"github.com/imattf/go-architecture/session"
 )
-
-// type key int
-
-// var userKey key = 0
-// var admin key = 1
 
 func main() {
 
-	//create parent background context
-	ctx := context.Background()
+	ctx := context.Background() //default parent
 
-	//create child context w/ 2 values
-	ctx = context.WithValue(ctx, "userID", 1234)
-
-	//append another value to the child context
-	ctx = context.WithValue(ctx, 1, "admin")
-
-	//pull the values out
-	val := ctx.Value("userID")
-	if val == nil {
-		fmt.Println("No value for key userID")
-	} else {
-		fmt.Println("userID =", val)
-	}
-
-	val = ctx.Value(1)
-	if val == nil {
-		fmt.Println("userID is not an admin")
-	} else {
-		fmt.Println("userID is an", val)
-	}
+	ctx = session.SetUserID(ctx, 42)      //go add value with child
+	ctx = session.SetUserAdmin(ctx, true) //go add value with child
+	i := session.GetUserID(ctx)           //go get context value
+	b := session.GetUserAdmin(ctx)        //go get context value
+	fmt.Printf("user %d is an admin: %t\n", i, b)
 
 }
--- session/session.go
+++ session/session.go
@@ -3,20 +3,36 @@ package session
 
 import "context"
 
-type key int
+type stringKey string
+type intKey int
 
-var userKey key = 0
+var userID stringKey
+var admin intKey
 
-//WithUserID is a...
-func WithUserID(ctx context.Context, userID int) context.Context {
-	return context.WithValue(ctx, userKey, userID)
+//SetUserID is a...
+func SetUserID(ctx context.Context, uID int) context.Context {
+	return context.WithValue(ctx, userID, uID)
 }
 
 //GetUserID is a...
-func GetUserID(ctx context.Context) *int {
-	userID, ok := ctx.Value(userKey).(int)
-	if !ok {
-		return nil
+func GetUserID(ctx context.Context) int {
+	uid := ctx.Value(userID)
+	if v, ok := uid.(int); ok {
+		return v
 	}
-	return &userID
+	return 0
+}
+
+//SetUserAdmin is a...
+func SetUserAdmin(ctx context.Context, isAdmin bool) context.Context {
+	return context.WithValue(ctx, admin, isAdmin)
+}
+
+//GetUserAdmin is a...
+func GetUserAdmin(ctx context.Context) bool {
+	b := ctx.Value(admin)
+	if v, ok := b.(bool); ok {
+		return v
+	}
+	return false
 }
